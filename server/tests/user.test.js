d59de2ae0d41e6589ea15a9b76e0f5db5500436f
Pablo <pablodss@gmail.com>
2016-12-12 08:12:28 -0200

    test: correct tests
    correcting user and auth api's tests

--- server/tests/user.test.js
+++ server/tests/user.test.js
@@ -18,38 +18,89 @@ after((done) => {
 });
 
 describe('## User APIs', () => {
-  let token;
-  const user = {
-    username: 'pablo',
-    password: 'rectius'
+  let user = {
+    username: 'username',
+    mobileNumber: '0123456789'
   };
 
-  describe('# POST /api/auth/login', () => {
-    it('should return a JWT token', (done) => {
+  describe('# POST /api/users', () => {
+    it('should create a new user', (done) => {
       request(app)
-        .post('/api/auth/login')
+        .post('/api/users')
         .send(user)
         .expect(httpStatus.OK)
         .then((res) => {
-          expect(res.body).to.have.property('username');
-          expect(res.body).to.have.property('token');
           expect(res.body.username).to.equal(user.username);
-          token = res.body.token;
+          expect(res.body.mobileNumber).to.equal(user.mobileNumber);
+          user = res.body;
           done();
         })
         .catch(done);
     });
   });
 
-  describe('# GET /api/auth/random-number', () => {
-    it('should get a random number', (done) => {
+  describe('# GET /api/users/:userId', () => {
+    it('should get user details', (done) => {
       request(app)
-        .get('/api/auth/random-number')
-        .set('Authorization', `Bearer ${token}`)
+        .get(`/api/users/${user._id}`)
         .expect(httpStatus.OK)
         .then((res) => {
-          expect(res.body).to.have.property('user');
-          expect(res.body.user.username).to.equal(user.username);
+          expect(res.body.username).to.equal(user.username);
+          expect(res.body.mobileNumber).to.equal(user.mobileNumber);
+          done();
+        })
+        .catch(done);
+    });
+
+    it('should report error with message - Not found, when user does not exists', (done) => {
+      request(app)
+        .get('/api/users/56c787ccc67fc16ccc1a5e92')
+        .expect(httpStatus.NOT_FOUND)
+        .then((res) => {
+          expect(res.body.message).to.equal('Not Found');
+          done();
+        })
+        .catch(done);
+    });
+  });
+
+  describe('# PUT /api/users/:userId', () => {
+    it('should update user details', (done) => {
+      user.username = 'KK';
+      request(app)
+        .put(`/api/users/${user._id}`)
+        .send(user)
+        .expect(httpStatus.OK)
+        .then((res) => {
+          expect(res.body.username).to.equal('KK');
+          expect(res.body.mobileNumber).to.equal(user.mobileNumber);
+          done();
+        })
+        .catch(done);
+    });
+  });
+
+  describe('# GET /api/users/', () => {
+    it('should get all users', (done) => {
+      request(app)
+        .get('/api/users')
+        .expect(httpStatus.OK)
+        .then((res) => {
+          expect(res.body).to.be.an('array');
+          done();
+        })
+        .catch(done);
+    });
+  });
+
+  describe('# DELETE /api/users/', () => {
+    it('should delete user', (done) => {
+      request(app)
+        .delete(`/api/users/${user._id}`)
+        .expect(httpStatus.OK)
+        .then((res) => {
+          expect(res.body.username).to.equal('KK');
+          expect(res.body.mobileNumber).to.equal(user.mobileNumber);
           done();
         })
         .catch(done);
